73a13db8a94c75c7b2b09820e85ac9c45bd4a011
Michael Recachinas <mgr3yp@virginia.edu>
2014-04-17 16:41:15 -0400

    changed alepnull -> gottagitgit

--- yolocups.py
+++ yolocups.py
@@ -18,7 +18,7 @@ def theory():
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template('404.html'), 404
+	return render_template('index.html'), 404
 
 if __name__ == '__main__':
 	app.run(debug=True)
