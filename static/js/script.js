5c89d203f86ef55b1480c13a6b6480c9a8c2319c
jasdev <js2wt@virginia.edu>
2014-04-29 00:18:18 -0400

    back to form post

--- static/js/script.js
+++ static/js/script.js
@@ -2,25 +2,13 @@ function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
+        $('.upload').attr('disabled','enabled');
+        reader.readAsDataURL(input.files[0]);
+
         reader.onload = function (e) {
             $('#blah')
                 .attr('src', e.target.result)
                 .show();
-            var form_data = $('#blah').attr('src', e.target.result);
-            $.ajax({
-                type: 'POST',
-                url: '/uploads',
-                data: form_data,
-                contentType: false,
-                cache: false,
-                processData: false,
-                async: false,
-                success: function(data) {
-                    console.log('Success!');
-                },
-            });
-            };
-            $('.upload').attr('disabled','enabled')
-            reader.readAsDataURL(input.files[0]);
+        };
     }
 }
--- yolocups.py
+++ yolocups.py
@@ -1,5 +1,4 @@
-import os
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -9,60 +8,49 @@ app.config['UPLOAD_FOLDER'] = 'uploads/'
 # These are the extension that we are accepting to be uploaded
 app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'gif'])
 
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
-'''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-'''
 
 @app.route('/')
 def yolocups():
-	return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/uploads', methods=['GET', 'POST'])
+
+@app.route('/uploads', methods=['POST'])
 def upload_file():
-	if request.method == 'POST':
-		file = request.files['file']
-		print "HELLO1"
-		if file and allowed_file(file.filename) and file.filename.split('.')[-1] in app.config['ALLOWED_EXTENSIONS']:
-			print "HELLO4"
-			filename = secure_filename(file.filename)
-			print "HELLO"
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			print "HELLO2"
-			return redirect(url_for('upload_file',filename=filename))
-	return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-'''
+    if request.method == 'POST':
+        file = request.files['file']
+
+        if file and allowed_file(file.filename) and file.filename.split('.')[-1] in app.config['ALLOWED_EXTENSIONS']:
+            filename = secure_filename(file.filename)
+
+            # Work with file here, redirect to a results page with processed image in template
+
+            return "Successfully uploaded " + str(filename)
+    return ''
+
 
 @app.route('/demo')
 def demo():
-	return render_template('demo.html')
+    return render_template('demo.html')
+
 
 @app.route('/theory')
 def theory():
-	return render_template('theory.html')
+    return render_template('theory.html')
+
 
 @app.route('/yolo')
 def yolo():
-	return redirect("http://www.youtube.com/watch?v=z5Otla5157c", code=302)
+    return redirect("http://www.youtube.com/watch?v=z5Otla5157c", code=302)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template('index.html'), 404
+    return render_template('index.html'), 404
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
